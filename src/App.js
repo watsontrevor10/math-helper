c5587757f8d589959103518aeddd7f4eea9d5a47
Trevor Watson <trevorwatson@Mac-mini.local>
2020-08-19 16:57:28 -0600

    basic game set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [mathProblem, setMathProblem] = useState("plus")
+  const [maxNum, setMaxNum] = useState(10);
+  const [firstNum, setFirstNum] = useState(0);
+  const [secNum, setSecNum] = useState(0);
+  const [answer, setAnswer] = useState("");
+  const [message, setMessage] = useState("Click Next");
+
+  const changeMaxNum = (e) => {
+    e.preventDefault();
+    setMaxNum(parseInt(e.target[0].value));
+  };
+
+  const userAnswer = (e) => {
+    e.preventDefault();
+    setAnswer(parseInt(e.target[0].value));
+    console.log(e.target[0].value);
+
+    if (answer === firstNum + secNum) {
+      setMessage("Correct!  You are the best!");
+    } else {
+      setMessage("Not quite, try again!");
+    }
+  };
+
+  const generateNums = () => {
+    setFirstNum(Math.floor(Math.random() * maxNum + 1));
+    setSecNum(Math.floor(Math.random() * maxNum + 1));
+    setAnswer("");
+    setMessage("Try a number");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Math Learner</h1>
       </header>
+      <div>
+        <h5>Select Math Problem</h5>
+      </div>
+      <div>
+        <label>
+          Select Math Problem {" "} | {" "}
+          <button>+</button>
+          <button>-</button>
+        </label>
+      </div>
+      <br/>
+      <div>
+        <div>
+          <label>
+            Set Max Number | {" "}
+            <input
+              name="maxNumber"
+              value={maxNum}
+              onChange={(e) => setMaxNum(parseInt(e.target.value))}
+            ></input>
+          </label>
+        </div>
+      </div>
+      <div>
+        <h3>{firstNum}</h3>
+        <h3>+</h3>
+        <h3>{secNum}</h3>
+        <h3>{message}</h3>
+      </div>
+      <div>
+        <form onSubmit={(e) => userAnswer(e)}>
+          <label>
+            Answer:{" "}
+            <input
+              name="answer"
+              id="name"
+              value={answer}
+              onChange={(e) => setAnswer(parseInt(e.target.value))}
+            ></input>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <button onClick={() => generateNums()}>Next</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
