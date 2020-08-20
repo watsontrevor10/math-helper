2b8575cc1020b9fc681ed39d6e33338fa578d8fb
Trevor Watson <trevorwatson@Mac-mini.local>
2020-08-19 22:23:01 -0600

    a lot of styling changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import { IconContext } from "react-icons";
+import { FaPlus, FaMinus } from "react-icons/fa";
 import "./App.css";
 
 const App = () => {
   const [mathProblem, setMathProblem] = useState("plus");
-  // const [mathSymbol, setMathSymbol] = useState("+");
   const [maxNum, setMaxNum] = useState(10);
   const [firstNum, setFirstNum] = useState(0);
   const [secNum, setSecNum] = useState(0);
@@ -22,7 +23,7 @@ const App = () => {
   const chnageSymbol = (e) => {
     e.preventDefault();
     setMathProblem(e.target.value);
-    generateNums()
+    generateNums();
   };
 
   // sets the users answer in state and changes the message to display depending on whether the user was correct or not
@@ -50,79 +51,104 @@ const App = () => {
   const generateNums = () => {
     const initialNum = Math.floor(Math.random() * maxNum + 1);
     const secondNum = Math.floor(Math.random() * maxNum + 1);
-    
+
     // checks to see if the user is doing subtraction and always puts the largest number on top to avoid negative numbers
     // if addition, sets values as normal
     if (mathProblem === "minus" && initialNum > secondNum) {
-      setFirstNum(initialNum)
-      setSecNum(secondNum)
+      setFirstNum(initialNum);
+      setSecNum(secondNum);
     } else {
       setFirstNum(secondNum);
       setSecNum(initialNum);
     }
 
     setAnswer("");
-    setMessage("Try a number");
+    setMessage("");
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Math Learner</h1>
+        <h1>Math Helper</h1>
       </header>
+
       {/* Initial problem setup selectors */}
-      <div>
-        <h5>Select Math Problem</h5>
-      </div>
-      <div>
-        <label>
-          Select Math Problem |
-          <button onClick={(e) => chnageSymbol(e)} value="plus">
-            +
-          </button>
-          <button value="minus" onClick={(e) => chnageSymbol(e)}>
-            -
-          </button>
-        </label>
-      </div>
-      <br />
-      <div>
-        <div>
-          <label>
-            Set Max Number |{" "}
-            <input
-              name="maxNumber"
-              value={maxNum}
-              // chnages state when user changes number
-              onChange={(e) => setMaxNum(parseInt(e.target.value))}
-            ></input>
-          </label>
+      <div className="Main-cont">
+        <div className="Selector-cont">
+          <div>
+            <IconContext.Provider value={{ size: "6em" }}>
+              <label>
+                Addition or Subtraction?
+                <br />
+                <button
+                  onClick={(e) => chnageSymbol(e)}
+                  value="plus"
+                  className="Plus-button"
+                >
+                  <FaPlus />
+                </button>
+              </label>
+              <button
+                value="minus"
+                onClick={(e) => chnageSymbol(e)}
+                className="Minus-button"
+              >
+                <FaMinus />
+              </button>
+            </IconContext.Provider>
+          </div>
+          <br />
+          <div>
+            <div>
+              <label>
+                Max Number{" "}
+                <input
+                  name="maxNumber"
+                  value={maxNum}
+                  // chnages state when user changes number
+                  onChange={(e) => setMaxNum(parseInt(e.target.value))}
+                ></input>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+        <div className="Results-cont">
+          {/* Answers displayed here */}
+          <div className="Results">
+            <div className="Answers-cont">
+              <h2>{firstNum}</h2>
+              <h2>
+                <ProblemSymbol />
+              </h2>
+              <h2>{secNum}</h2>
+              <h2>=</h2>
+            </div>
+          </div>
+          {/* Answer form for user to submit answers and load the next math problem */}
+          <div className="Submit-cont">
+            <form onSubmit={(e) => userAnswer(e)}>
+              <input
+                name="answer"
+                id="name"
+                placeholder="Answer"
+                value={answer}
+                onChange={(e) => setAnswer(parseInt(e.target.value))}
+              ></input>
+              <br />
+              <input type="submit" value="Submit" className="Submit-button" />
+              <button onClick={() => generateNums()} className="Submit-button">
+                Next
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-      {/* Answers displayed here */}
       <div>
-        <h3>{firstNum}</h3>
-        <h3>
-          <ProblemSymbol />
-        </h3>
-        <h3>{secNum}</h3>
-        <h3>{message}</h3>
-      </div>
-      {/* Answer form for user to submit answers and load the next math problem */}
-      <div>
-        <form onSubmit={(e) => userAnswer(e)}>
-          <label>
-            Answer:{" "}
-            <input
-              name="answer"
-              id="name"
-              value={answer}
-              onChange={(e) => setAnswer(parseInt(e.target.value))}
-            ></input>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        <button onClick={() => generateNums()}>Next</button>
+        <div>
+          <h3 className="Message">{message}</h3>
+        </div>
       </div>
     </div>
   );
